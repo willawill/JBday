cf9b8d78a6c4b554f5b793558e58b2a629f1642e
Willa Wang <willa@greenhouse.io>
2016-04-09 20:11:56 -0400

    Adding footer

--- Jbday.elm
+++ Jbday.elm
@@ -2,12 +2,31 @@ module Jbday where
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (..)
 
 navHeader : Html
 navHeader = 
   header
     [ id "header" ]
-    [ h1 [] [ text "FINALLY" ]
+    [ h1 [] [ text "NAV" ]
     ]
 
-main = div [] [ navHeader ]
+firstPage :Html
+firstPage = 
+  div [ id "video-bg"]
+  [
+    p [] [ text "HELLO JAY"]
+  ]
+
+infoFooter : Html
+infoFooter = 
+  footer
+    [ id "info-footer" ]
+    [ p [] [ text "footer" ],
+      p [] [ text "footer" ],
+      p [] [ text "footer" ]      
+    ]
+
+
+
+main = div [] [ navHeader, firstPage, infoFooter ]
